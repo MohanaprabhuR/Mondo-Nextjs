a3280c941c6e641dfd96fa3e389dcfebea6e24ea
Mohanaprabhu.R <mohan@timeless.co>
2025-01-30 12:47:44 +0530

    Next config file chnaged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import HeroCarousel from "@/Components/HeroSection/herocarousal";
-import ShowCarousal from "@/Components/ShowCarousal/ShowCarousal";
+// import ShowCarousal from "@/Components/ShowCarousal/ShowCarousal";
 import GenreCarouselItem from "@/Components/ShowCarousal/CategoryCarousal";
 
 export default async function Shows() {
@@ -15,7 +15,7 @@ export default async function Shows() {
     <>
       <HeroCarousel shows={shows.data} />
       <section className="mx-auto px-4 sm:px-6 lg:px-8 xl:max-w-[1280px] flex flex-col space-y-10 pb-[18px] pt-[50px] sm:space-y-12 sm:overflow-visible sm:pb-[107px]">
-        <ShowCarousal shows={shows.data} />
+        {/* <ShowCarousal shows={shows.data} /> */}
         {generdetails.data?.map((genre: never, index: number) => (
           <GenreCarouselItem key={index} genre={genre} allShows={shows.data} />
         ))}
